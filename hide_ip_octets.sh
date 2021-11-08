2add6a35bada5a25182fd6848410d74dcd9182c5
moloch291 <danielkomjati1@gmail.com>
2021-11-08 14:41:27 +0100

    refactor the script which hides the first three octets of each ip adress in log file

--- hide_ip_octets.sh
+++ hide_ip_octets.sh
@@ -1,6 +1,31 @@
-LOG_CONTENT=$(cat access.log);
-COUNTER=0
-while read line; do
-    COUNTER=$(( COUNTER+1 ))
-    echo "$line" | sed 's/[^.]\+/XXX/1; s/[^.]\+/XXX/2; s/[^.]\+/XXX/3;' >> copy.log
-done < access.log
+#!/bin/bash
+
+inform_user_start() {
+    echo 'Hiding ips...'
+}
+
+inform_user_finish() {
+    echo 'Ips hidden! Check: copy.log in pwd!'
+}
+
+hiding_ips() {
+    LOG_CONTENT=$(cat access.log);
+    COUNTER=0;
+    while read line; do
+        NEW_LINE=$(echo "$line" | sed 's/[^.]\+/XXX/1; s/[^.]\+/XXX/2; s/[^.]\+/XXX/3;')
+        if [ $COUNTER -eq 0 ]; then
+            echo "$NEW_LINE" > ips_hidden.log
+        else
+            echo "$NEW_LINE" >> ips_hidden.log
+        fi
+        COUNTER=$(( COUNTER+1 ))
+    done < access.log
+}
+
+main() {
+    inform_user_start
+    hiding_ips
+    inform_user_finish
+}
+
+main
