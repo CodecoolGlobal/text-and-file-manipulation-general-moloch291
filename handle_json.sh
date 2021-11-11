834d53bf1a8bdc1c6d00f9f685ad039463cc9c1c
moloch291 <danielkomjati1@gmail.com>
2021-11-11 17:03:47 +0100

    refactor json handling

--- handle_json.sh
+++ handle_json.sh
@@ -5,29 +5,16 @@ extract_first_names() {
     jq '.employees | .[].firstName' employees.json
 }
 
-extract_first_names_with_phone_number() {
-    echo "\nNames and numbers:"
-    LENGTH=$(jq '.employees | length' employees.json)
-    CURRENT_EMPLOYEE=0
-    while [ $CURRENT_EMPLOYEE -le $(( LENGTH - 1 )) ]; do
-        echo "\nEmployee number "$(( $CURRENT_EMPLOYEE + 1 ))":\nName:"
-        jq '.employees | .['$CURRENT_EMPLOYEE'].firstName' employees.json
-        echo "Phone:"
-        jq '.employees | .['$CURRENT_EMPLOYEE'].phoneNumbers | .[].number' employees.json
-        CURRENT_EMPLOYEE=$(( CURRENT_EMPLOYEE + 1 ))
-    done
-}
-
-extract_first_name_and_cell_number() {
+extract_first_name_and_number() {
     # ToDo_: implement me!
-    echo "\nNames and mobile:"
+    echo "\nNames and $1:"
     LENGTH=$(jq '.employees | length' employees.json)
     CURRENT_EMPLOYEE=0
     while [ $CURRENT_EMPLOYEE -le $(( LENGTH - 1 )) ]; do
-        echo "\nEmployee number "$(( $CURRENT_EMPLOYEE + 1 ))":\nName:"
+        echo "\nEmployee "$(( $CURRENT_EMPLOYEE + 1 ))":\nName:"
         jq '.employees | .['$CURRENT_EMPLOYEE'].firstName' employees.json
-        echo "Mobile:"
-        jq '.employees | .['$CURRENT_EMPLOYEE'].phoneNumbers | .[1].number' employees.json
+        echo "$1:"
+        jq '.employees | .['$CURRENT_EMPLOYEE'].phoneNumbers | '$3'' employees.json
         CURRENT_EMPLOYEE=$(( CURRENT_EMPLOYEE + 1 ))
     done
 }
@@ -36,9 +23,9 @@ main() {
     if [ "$1" = "names" ]; then
         extract_first_names
         elif [ "$1" = "names-and-numbers" ]; then
-        extract_first_names_with_phone_number
+        extract_first_name_and_number "phone numbers" "Phone" ".[].number"
         elif [ "$1" = "names-and-mobiles" ]; then
-        extract_first_name_and_cell_number
+        extract_first_name_and_number "mobile" "Mobile" ".[1].number"
     else
         echo "Pleaes use the following options: names, names-and-numbers or names-and-mobiles"
     fi
